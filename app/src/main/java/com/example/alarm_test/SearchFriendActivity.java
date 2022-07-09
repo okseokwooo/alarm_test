cd6beb09f87cf16bc3ec6a19da4d7fe323bc76cb
[유저 아이디] <[oswzxc@naver.com]>
2022-07-10 02:52:47 +0900

    3

--- app/src/main/java/com/example/alarm_test/SearchFriendActivity.java
+++ app/src/main/java/com/example/alarm_test/SearchFriendActivity.java
@@ -3,28 +3,28 @@ package com.example.alarm_test;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import com.example.alarm_test.data.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class SearchFriendActivity extends AppCompatActivity {
     Button searchBtn;
     EditText searchName;
+    TextView friend_Tv;
     private String name;
     private DatabaseReference mdbr;
     private Map<String , Object> childUpdates = null;
@@ -37,21 +37,34 @@ public class SearchFriendActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_friend);
         searchBtn = findViewById(R.id.searchBtn);
         searchName = findViewById(R.id.search_Tv);
-
+        friend_Tv = findViewById(R.id.friend_Tv);
+        friend_Tv.setVisibility(View.INVISIBLE);
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 name =  searchName.getText().toString();
                 user = FirebaseAuth.getInstance().getCurrentUser();
 
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("UserAccount");
-                myRef.addValueEventListener(new ValueEventListener() {
+                FirebaseDatabase.getInstance().getReference().addValueEventListener(new ValueEventListener() {
                     @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        User user = snapshot.getValue(User.class);
-                        System.out.println("user = " + user);
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            for(DataSnapshot ds : snapshot.getChildren()){
+                                String id = (String) ds.child("ID").getValue();
+                                if(id.equals(name)){
+                                    friend_Tv.setText(id);
+                                    friend_Tv.setVisibility(View.VISIBLE);
+                                    friend_Tv.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+
+                                        }
+                                    });
+                                }
+                            }
+                        }
                     }
+
 
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
